31ffeef286fedbb34f31e86a76af20e6b4a34ab6
Modrarus <modrarus@outlook.com>
2019-08-07 19:33:11 +0500

    Инициализация

---
+++ src/main/java/org/modrarus/widget/service/WidgetServiceApplication.java
@@ -0,0 +1,13 @@
+package org.modrarus.widget.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WidgetServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WidgetServiceApplication.class, args);
+	}
+
+}
